fde5d7ef3870dfdaa9a3faac3a47ba62b95230f5
Jimenez <octavio.jimenez@hpe.com>
2016-08-15 12:13:34 -0600

    simple note rendering and base 'add task' button

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,44 @@
-import React, { Component } from 'react';
-import Note from './Note.jsx'
+import uuid from 'node-uuid';
+import React, {Component} from 'react';
+import Notes from './Notes.jsx'
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            notes: [
+                {
+                    id: uuid.v4(),
+                    task: 'Review Webpack'
+                }, {
+                    id: uuid.v4(),
+                    task: 'Learn React'
+                }, {
+                    id: uuid.v4(),
+                    task: 'Learn React-Redux'
+                }
+            ]
+        };
+        // experimental feature
+        // this.addNote = this.addNote.bind(this);
+    }
+    addNote = () => {
+        this.setState({
+            notes: this.state.notes.concat([
+                {
+                    id: uuid.v4(),
+                    task: 'New task'
+                }
+            ])
+        });
+    }
     render() {
-        return <Note/>;
+        const notes = this.state.notes;
+        return (
+            <div>
+                <button onClick={this.addNote}>Add note</button>
+                <Notes notes={notes}/>
+            </div>
+        );
     }
 }
---
+++ app/components/Notes.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Note from './Note.jsx'
+
+export default({notes}) =>
+    <ul>{notes.map(note => <li key={note.id}>
+        <Note task={note.task}/></li>)}</ul>
